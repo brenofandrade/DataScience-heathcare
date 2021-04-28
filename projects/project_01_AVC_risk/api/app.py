1f4dae293b4a73c6403f92228b55bb6f5e3f6c3b
Breno.Andrade <andradefbreno@gmail.com>
2021-04-28 11:37:39 -0300

    api previsao

--- projects/project_01_AVC_risk/api/app.py
+++ projects/project_01_AVC_risk/api/app.py
@@ -1,24 +1,95 @@
 from flask import Flask, request, render_template, redirect, url_for
 import numpy as np
-
-
+import pandas as pd
+import pickle
 
 app = Flask(__name__)
-@app.route('/success/<idade>')
-def success(idade):
-    return 'Sua idade é: %s' % idade
+def predict_model(x_data):
+    model = pickle.load(open('../data_process/model/modelo.pkl', 'rb'))
+    resultado = model.predict_proba(x_data)
+    return resultado
 
 @app.route('/',methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
-        idade = request.form['IDADE']
-        sexo = request.form['SEXO']
-        imc = request.form['IMC']
+        
+        sexo = int(request.form['sexo'])
 
-        x_input = np.array([idade, sexo, imc])
+        idade = float(request.form['idade'])
+        
+        hipertensao = int(request.form['hipertensao'])
 
-        print(x_input)
-        return f'Sua idade é: {x_input}'
+        coracao = request.form['doenca_coracao']
+
+        residencia = request.form['residencia']
+
+        glicose = request.form['glicose_media']
+
+        imc = float(request.form['imc'])
+
+        trabalho = request.form['trabalho']
+
+        tabagismo = request.form['tabagismo']
+        
+
+        trab_autonomo = 0
+        trab_nunca = 0
+        trab_privado = 0
+        trab_publico = 0
+        trab_crianca = 0
+
+        if trabalho == '1':
+            trab_autonomo = 1
+        elif trabalho == '2':
+            trab_privado = 1
+        elif trabalho == '3':
+            trab_publico = 1
+        elif trabalho == '4':
+            trab_nunca = 1
+        elif trabalho == '5':
+            trab_crianca = 1
+        else:
+            print("Unexpected Error!")
+            
+        
+
+
+
+
+        novo_paciente = pd.DataFrame(
+            {
+            'sexo': sexo,
+            'idade': idade,
+            'hipertensao': hipertensao,
+            'doenca_coracao' : coracao,
+            'tipo_residencia' : residencia,
+            'media_nivel_glicose' : glicose,
+            'imc' : imc,
+            'trabalho_Autonomo' : 0,
+            'trabalho_Never_worked': 0,
+            'trabalho_Privado': 0,
+            'trabalho_Servidor_Publico': 0,
+            'trabalho_crianca': 0,
+            'tabagismo_desconhecido': 0,
+            'tabagismo_fuma': 0,
+            'tabagismo_fuma_eventualmente': 0,
+            'tabagismo_nunca_fumou': 0
+            },
+            index=[0]
+        )
+
+        if tabagismo == '1':
+            novo_paciente['tabagismo_fuma'][0] = 1
+        elif tabagismo == '2':
+            novo_paciente['tabagismo_fuma_eventualmente'][0] = 1
+        elif tabagismo == '0':
+            novo_paciente['tabagismo_nunca_fumou'][0] = 1
+        elif tabagismo == 'na':
+            novo_paciente['tabagismo_desconhecido'][0] = 1
+
+        risco_avc = predict_model(novo_paciente)
+
+        return f'O risco de AVC é {risco_avc[0][1]} %'
     return render_template('home.html')
     
 
